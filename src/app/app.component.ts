119970ea7655b53877f6f81aec4298ee87611333
DevindiSiwurathna <devindisiwurathna@gmail.com>
2023-03-30 23:24:56 +0530

    ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'StructuralDirectives';
+  isApproved=false
+
+  approve(status:boolean){
+    this.isApproved=status;
+  }
 }
